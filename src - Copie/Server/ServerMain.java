efa1e2f5d7f880be2185a9394fccb8a17b5e09f6
Wissal Belbachir <belbachirwissal3@gmail.com>
2025-10-27 17:22:39 +0100

    Ajout complet du TP4 avec exécutables

---
+++ src - Copie/Server/ServerMain.java
@@ -0,0 +1,51 @@
+package Server;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import HelloWorldApp.Hello;
+import HelloWorldApp.HelloHelper;
+import Servant.HelloServant;
+
+public class ServerMain {
+public static void main(String []args){
+    try {
+      
+        ORB orb = ORB.init(args, null);
+        
+       
+        POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+        rootpoa.the_POAManager().activate();
+        
+      
+        HelloServant helloServant = new HelloServant();
+        helloServant.setOrb(orb);
+        
+  
+        org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloServant);
+        Hello href = HelloHelper.narrow(ref);
+        
+     
+        org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+ 
+        String name = "Hello";
+        NameComponent path[] = ncRef.to_name(name);
+        ncRef.rebind(path, href);
+        
+        System.out.println("HelloWorldServer ready and waiting ...");
+        for(;;){ 
+               orb.run();
+        }
+        
+       
+    } catch (Exception e) {
+        System.err.println("ERROR: " + e);
+        e.printStackTrace(System.out);
+    }
+}
+}
